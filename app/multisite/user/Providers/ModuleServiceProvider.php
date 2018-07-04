95094c2e6218fa2fed8337150ca1c5055312c6e9
Sahak <hakobyan.sahak88@gmail.com>
2018-07-04 22:59:38 +0400

    update

---
+++ app/multisite/user/Providers/ModuleServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\multisite\user\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'mini');
+
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
