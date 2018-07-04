ffd2a2befb99c9be7829aa1954cb731b55c748df
sahakavatar <hakobyan.sahak88@gmail.com>
2018-07-04 17:48:24 +0400

    profile left unit

---
+++ resources/units/profile_left_unit/demo.blade.php
@@ -0,0 +1,16 @@
+<div class="col-md-3">
+    <div class="profile-left-menu">
+        <div class="profile-img">
+            <img src="https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&h=350" alt="img">
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="">General</a></li>
+                <li><a href="">Pages</a></li>
+                <li><a href="">My Media</a></li>
+                <li><a href="">Uploads</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
