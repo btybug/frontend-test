b19f62df64b378420661977b99166ae0b17ebf1c
sahakavatar <hakobyan.sahak88@gmail.com>
2018-07-04 17:11:12 +0400

    unit profile

---
+++ resources/units/profile_unit/demo.blade.php
@@ -0,0 +1,72 @@
+<div class="container-fluid">
+    <div class="user-details">
+        <div class="row">
+            <div class="col-md-2">
+                <div class="user-img">
+                    <img src="https://www.onlineuzaktanegitim.com/uploads/users/1.png" alt="img">
+                </div>
+
+            </div>
+            <div class="col-md-10">
+                <div class="details">
+                    <div>
+                        <h2>Profile</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet asperiores cum distinctio
+                            dolorem,
+                            enim fugiat, labore nesciunt optio recusandae, repellendus sit velit voluptatem voluptates?
+                            A
+                            architecto dicta quasi quo vel.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="tab" href="#home">Tab 1</a></li>
+        <li><a data-toggle="tab" href="#menu1">Blog (all posts)</a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div id="home" class="tab-pane fade in active">
+            <div class="row dis-flex">
+                <div class="col-md-2 col-xs-12">
+                    <div class="ads">
+                        Our Ads
+                    </div>
+                </div>
+                <div class="col-md-8 col-xs-12">
+                    <div class="posts">
+                        <div class="row dis-flex">
+                            <div class="col-md-6 col-xs-12">
+                                <div class="post">
+                                    post1
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda blanditiis dolor, earum facere, fugiat id nulla possimus quae quidem vel veritatis voluptates. Aperiam, commodi debitis eius necessitatibus obcaecati perferendis tenetur.
+                                </div>
+
+                            </div>
+                            <div class="col-md-6 col-xs-12">
+                                <div class="post">
+                                    post2
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad, aliquam, asperiores assumenda autem blanditiis dignissimos eius esse ex expedita fugiat magni nam praesentium quisquam repellat soluta tempore temporibus voluptatum!
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 col-xs-12">
+                    <div class="ads">
+                        Our Ads
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="menu1" class="tab-pane fade">
+            <h3>Menu 1</h3>
+            <p>Some content in menu 1.</p>
+        </div>
+
+    </div>
+</div>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
