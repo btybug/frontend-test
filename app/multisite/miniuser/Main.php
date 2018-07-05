cd0e25954a24d2c9ac41c7c5668ccbdebaa825ad
sahakavatar <hakobyan.sahak88@gmail.com>
2018-07-05 17:20:07 +0400

    up

---
+++ app/multisite/miniuser/Main.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: menq
+ * Date: 04.07.2018
+ * Time: 16:09
+ */
+
+namespace App\multisite\miniuser;
+
+
+use App\multisite\miniuser\Providers\ModuleServiceProvider;
+use Btybug\User\User;
+use Illuminate\Http\Request;
+
+class Main
+{
+    public function run(User $user,Request $request)
+    {
+        app()->register(ModuleServiceProvider::class);
+        $this->user = $user;
+        return $this->account($user,$request);
+
+    }
+    public function account($user,$request){
+        return view('mini::account',compact('user'));
+    }
+}
