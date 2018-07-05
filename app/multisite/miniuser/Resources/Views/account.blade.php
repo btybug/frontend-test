7859baeeff680c41fe7a6ad9ede1586aa95fd9cf
Sahak <hakobyan.sahak88@gmail.com>
2018-07-05 23:40:36 +0400

    up

---
+++ app/multisite/miniuser/Resources/Views/account.blade.php
@@ -0,0 +1,5 @@
+@extends('mini::layouts.app')
+@section('content')
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi dolore, earum eligendi eos eveniet ex explicabo illo, iste laudantium numquam officia porro praesentium quisquam sint sunt unde veniam vitae?</div>
+@endsection
