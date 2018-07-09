972de4e74625c1046ea722606bad0aaee9135597
Sahak <hakobyan.sahak88@gmail.com>
2018-07-09 23:11:19 +0400

    up

--- vendor/btybug/btybug/src/resources/views/layouts/_partials/frontend/head.blade.php
+++ vendor/btybug/btybug/src/resources/views/layouts/_partials/frontend/head.blade.php
@@ -1,6 +1,21 @@
 <meta charset="utf-8">
-<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Multi Site Management System">
+<meta name="author" content="Abokamal">
+<meta name="keywords" content=" mobile applications, web app,web design, multisite, my-site, site, cv,cms">
+<meta property="og:url" content="http://btybug.com">
+<meta property="og:type" content="Multi Site">
+<meta property="og:title" content="Btybug Multy Site Management System">
+<meta property="og:description" content="Btybug Multy Site Management System">
+<meta property="og:image" content="https://freepngimg.com/download/bugs/7-2-bug-png-6.png">
+<meta name="geo.region" content="UK">
+<meta name="geo.region" content="GB" />
+<meta name="geo.placename" content="London" />
+<meta name="geo.position" content="51.486024;-0.137304" />
+<meta name="ICBM" content="51.486024, -0.137304" />
+
+
 <title>@yield('title')</title>
 @yield('metas')
 <link type="image/x-icon" rel="icon" href="{{ asset('assets/favicon.ico') }}"/>
