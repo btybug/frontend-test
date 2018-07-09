80eae97702274c7af33b0e0d10734afb67aa2d0b
sahakavatar <hakobyan.sahak88@gmail.com>
2018-07-09 10:57:43 +0400

    pages

--- app/multisite/miniuser/Resources/Views/mysite/pages.blade.php
+++ app/multisite/miniuser/Resources/Views/mysite/pages.blade.php
@@ -1,48 +1,116 @@
 @extends('mini::layouts.app')
 @section('content')
-   <div class="row col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+    <div class="row col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 
-      <div class="col-md-12 pull-right">
-         {!! Form::open(['route' => "mini_page_create"]) !!}
-         <div class="form-group">
-            <div class="col-md-3">
-               <input type="text" name="title" class="form-control">
-            </div>
-         </div>
-         <div class="form-group">
-            <div class="col-md-3">
-               {{ Form::button('<i class="fa fa-plus" aria-hidden="true"></i> New Page', array('type' => 'submit', 'class' => 'pull-right create_new_btn m-l-20')) }}
-            </div>
-         </div>
-         {!! Form::close() !!}
-      </div>
-   </div>
-   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-      <article>
-         <div class="bb-menu-container">
-            <div class="col-md-8 col-md-offset-2 bb-menu-group-body">
-               <ol class="bb-menu-area">
-                  {!! hierarchyMiniUserPagesListFull($pages) !!}
-               </ol>
-            </div>
-         </div>
-      </article>
-   </div>
+        <div class="col-md-12 pull-right">
+            {!! Form::open(['route' => "mini_page_create"]) !!}
+            <div class="form-group">
+                <div class="col-md-3">
+                    <div class="row">
+                        <input type="text" name="title" class="form-controll account">
+                        <div class="button">
+                            {{ Form::button('<i class="fa fa-plus" aria-hidden="true"></i> New Page', array('type' => 'submit', 'class' => ' create_new_btn')) }}
+                        </div>
+                    </div>
 
-   @include('btybug::_partials.delete_modal')
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <article class="mt-4">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="my-account-left-col">
+                        <div class="bb-menu-container">
+                            <div class="bb-menu-group-body">
+                                <ul class="bb-menu-area">
+                                    {!! hierarchyMiniUserPagesListFull($pages) !!}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="my-account-right-col">
+                        <div class="heading">
+                            <ol class="breadcrumb pull-right">
+                                <li class="breadcrumb-item"><a href="javascript:;">Edit</a></li>
+                                <li class="breadcrumb-item active">settings</li>
+                                <li class="breadcrumb-item active">preview</li>
+                            </ol>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="status mb-5 mt-5">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <label>Status</label>
+                                </div>
+                                <div class="col-md-4">
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="access">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <label>Access</label>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="checkaccess">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-check form-check-inline">
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1"
+                                                               value="option1">Public
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 text-right">
+                                                <div class="form-check form-check-inline">
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                                               value="option2">Members
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="area-preview">
+preview area
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </article>
+    </div>
+
+    @include('btybug::_partials.delete_modal')
 @stop
 
 @section('css')
-   {!! HTML::style('public/css/create_pages.css') !!}
-   {!! HTML::style('public/css/tool-css.css?v=0.23') !!}
-   {!! HTML::style('public/css/page.css?v=0.15') !!}
-   {!! HTML::style('public/css/admin_pages.css') !!}
+    {!! HTML::style('public/css/create_pages.css') !!}
+    {!! HTML::style('public/css/tool-css.css?v=0.23') !!}
+    {!! HTML::style('public/css/page.css?v=0.15') !!}
+    {!! HTML::style('public/css/admin_pages.css') !!}
+    {!! HTML::style('public/minicms/main.css') !!}
 
 @stop
 
 
 @section('js')
-   {!! HTML::script('public/js/create_pages.js') !!}
-   {!! HTML::script('public/js/admin_pages.js') !!}
-   {!! HTML::script('public/js/menus.js') !!}
+    {!! HTML::script('public/js/create_pages.js') !!}
+    {!! HTML::script('public/js/admin_pages.js') !!}
+    {!! HTML::script('public/js/menus.js') !!}
 @stop
