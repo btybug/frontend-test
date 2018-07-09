32037dc8897158c1573556d6fccaf809bfac328d
terakyan <edo.terakyan@gmail.com>
2018-07-09 16:10:26 +0400

    up

---
+++ resources/views/ContentLayouts/front_layout_with_2_8_2_col/demo.blade.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+
+        </div>
+        <div class="col-md-8">
+            {!! main_content($settings) !!}
+        </div>
+        <div class="col-md-2">
+
+        </div>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css/style.css') !!}
