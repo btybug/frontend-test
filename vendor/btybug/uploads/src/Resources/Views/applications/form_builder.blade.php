e629214e47bc125e998b8a46409c9c89f0660ab1
Eduard <edo.terakyan@gmail.com>
2018-07-11 21:37:29 +0400

    up

--- vendor/btybug/uploads/src/Resources/Views/applications/form_builder.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('btybug::layouts.admin')
-@section('content')
-    form builder here
-@stop
-@section('CSS')
-
-@stop
-@section('JS')
-@stop
--- vendor/btybug/uploads/src/Resources/Views/applications/index.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/applications/index.blade.php
@@ -1,4 +1,80 @@
 @extends('btybug::layouts.admin')
 @section('content')
-dddddddddddddddddddd
+    <div class="row">
+
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 cms_module_list">
+            <h3 class="menuText f-s-17">
+                <span class="module_icon_main"></span>
+            </h3>
+            <hr>
+            <ul class="list-unstyled menuList" id="components-list">
+                <li class=" active">
+                    <a href="#"> <span
+                                class="module_icon"></span> TEST</a>
+                </li>
+                {{--@foreach($plugins as $plugin)--}}
+                    {{--<li class=" @if($selected->name==$plugin['name']) active @endif ">--}}
+                        {{--<a href="{!! route('core_packages',['p'=>$plugin['name']]) !!}"> <span--}}
+                                    {{--class="module_icon"></span> {!! $plugin['name'] !!}</a>--}}
+                    {{--</li>--}}
+                {{--@endforeach--}}
+            </ul>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+            <div class="row">
+                <div class="col-xs-12">
+                    @if(@$selected)
+                        <div class="module_detail">
+                            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                                <img src="{!! url('public/images/module.jpg') !!}" alt=""
+                                     class="img-rounded img-responsive"/>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                <div class="module-title"> name:{!! $selected->name or null !!}</div>
+                                <div class="module-title"> version:{!! $selected->version or null !!}</div>
+                                <div class="module-desc">
+                                    {!! $selected->description or null !!}
+                                </div>
+
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 pull-right text-right m-t-20">
+                                {{--@if(isset($module->have_setting) and $module->have_setting==1)--}}
+                                <a href="{!! url('#/setting','id') !!}"
+                                   class="btn  btn-sm  m-b-5 p-l-20 p-r-20 width-150 text-left settings"><i
+                                            class="fa fa-pencil f-s-14 m-r-10"></i> Settings</a>
+                                {{--@endif--}}
+                                <a href="{!! url('admin/uploads/modules/'.$selected->name.'/explore') !!}"
+                                   class="btn  btn-sm  m-b-5 p-l-20 p-r-20 width-150 text-left"><i
+                                            class="fa fa-cogs f-s-14 m-r-10"></i>Explore</a>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+
+                        <div class="module_detail_link">
+                            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 m-t-10 m-b-10">
+                                <a href="{!! 'Author' !!}"
+                                   class="module_detail_author_link">{!!'Site Url' !!}</a>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 module_author_detail m-t-10 m-b-10">
+                                <div class="pull-left">
+                                    <i class="fa fa-bars f-s-15" aria-hidden="true"></i>
+                                    Version {!! 'version' !!}
+                                </div>
+                                <div class="pull-right">
+                                    <i class="fa fa-user f-s-15" aria-hidden="true"></i>
+                                    {!! 'Author'!!}
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('CSS')
+    {!! HTML::style('public/css/new-store.css') !!}
 @stop
