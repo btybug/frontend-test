34d4e590f5bd01098a9663b78c0c4751c2786caa
Vardan <vardandanielyan@gmail.com>
2018-07-12 17:02:01 +0400

    new

--- vendor/btybug/uploads/src/Resources/Views/applications/index.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/applications/index.blade.php
@@ -2,10 +2,8 @@
 @section('content')
     <div class="container">
             <h2>Forms</h2>
-        @if(isset($editableData))
-            <textarea class="hidden" value="{{$editableData}}"></textarea>
-        @endif
-        <a href="application/form-builder"><button type="button" class="btn btn-success creat">Creat New</button></a>
+
+        <a href="{!! route('application_form') !!}"><button type="button" class="btn btn-success creat">Creat New</button></a>
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -23,8 +21,8 @@
                     <td>{{$val->title}}</td>
                     <td>{{$val->description}}</td>
                     <td>
-                        <a href="application/delete/{{$val->id}}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
-                        <a href="application/edit/{{$val->id}}"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
+                        <a href="{!! route('application_form_delete',$val->id) !!}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
+                        <a href="{!! route('application_form_edit',$val->id) !!}"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
                     </td>
                 </tr>
                 @endforeach
