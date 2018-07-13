0954e9fb026732d0a56ba54b82e5b9ccfcac3066
Vardan <vardandanielyan@gmail.com>
2018-07-13 15:41:18 +0400

    buttons

--- vendor/btybug/uploads/src/Resources/Views/applications/index.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/applications/index.blade.php
@@ -21,10 +21,9 @@
                     <td>{{$val->title}}</td>
                     <td>{{$val->description}}</td>
                     <td>
-
-                        <a href="{!! route('application_form_delete',$val->id) !!}"><span class="glyphi glyphicon glyphicon-trash" aria-hidden="true"></span></a>
-                        <a href="{!! route('application_form_edit',$val->id) !!}"><span class="glyphi glyphicon glyphicon-edit" aria-hidden="true"></span></a>
-                        <a href="{!! route('application_form_view',$val->id) !!}"><span class="glyphi glyphicon glyphicon-eye-open" aria-hidden="true"></span></a>
+                        <a class="pull-right btn btn-danger" href="{!! route('application_form_delete',$val->id) !!}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
+                        <a class="pull-right btn btn-warning" href="{!! route('application_form_edit',$val->id) !!}"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
+                        <a class="pull-right btn btn-info" href="{!! route('application_form_view',$val->id) !!}"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></a>
                     </td>
                 </tr>
                 @endforeach
