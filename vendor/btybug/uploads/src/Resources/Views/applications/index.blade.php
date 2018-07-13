03200f697ed1ade453bcca4b34cb0c8a73326005
Vardan <vardandanielyan@gmail.com>
2018-07-13 14:59:34 +0400

    new3

--- vendor/btybug/uploads/src/Resources/Views/applications/index.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/applications/index.blade.php
@@ -21,9 +21,10 @@
                     <td>{{$val->title}}</td>
                     <td>{{$val->description}}</td>
                     <td>
-                        <a href="{!! route('application_form_view',$val->id) !!}"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></a>
+
                         <a href="{!! route('application_form_delete',$val->id) !!}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
                         <a href="{!! route('application_form_edit',$val->id) !!}"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
+                        <a href="{!! route('application_form_view',$val->id) !!}"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></a>
                     </td>
                 </tr>
                 @endforeach
