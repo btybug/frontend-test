c1f0980c497e0bdf0115a3191f98a0d4b92196fe
sahakavatar <hakobyan.sahak88@gmail.com>
2018-07-18 11:42:09 +0400

    unit resume

--- resources/units/unit_urkis_resume/tpl.blade.php
+++ resources/units/unit_urkis_resume/tpl.blade.php
@@ -1,11 +1,69 @@
-<div class="profile-main {{(isset($settings['style'])&& $settings['style'] ) ? $settings['style'] : 'demo-column'}}">
-    @if(isset($settings['sections']))
-        @foreach($settings['sections'] as $section)
-            {!! BBRenderUnits($section) !!}
-        @endforeach
-    @endif
-</div>
-<div class="clearfix"></div>
+<!-- Resume Area Starts -->
+<section id="resume" class="resume-area section-padding">
+    <div>
+        <div class="row">
+            <div class="col-md-12 resume">
+                <header class="section-header text-center">
+                    <p class="slugan">More about my past</p>
+                    <h2>Resume</h2>
+                </header>
+                <div class="row">
+                    <div class="col-md-6 education-bg">
+                        <div class="item-heading d-flex justify-content-start align-items-center">
+                            <div class="hicom">
+                                <i class="fa fa-graduation-cap"></i>
+                            </div>
+                            <h3 class="single-ititle">education</h3>
+                        </div>
 
+                        <div class="single-timeline">
+                            <p class="years">2015-2016</p>
+                            <h4 class="timeline-title">Computer Science</h4>
+                            <p class="timeline-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                        </div>
+                        <div class="single-timeline">
+                            <p class="years">2015-2016</p>
+                            <h4 class="timeline-title">Computer Science</h4>
+                            <p class="timeline-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                        </div>
+                        <div class="single-timeline">
+                            <p class="years">2015-2016</p>
+                            <h4 class="timeline-title">Computer Science</h4>
+                            <p class="timeline-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6 education-bg employment-history">
+                        <div class="item-heading d-flex justify-content-start align-items-center">
+                            <div class="hicom">
+                                <i class="fa fa-shopping-bag"></i>
+                            </div>
+                            <h3 class="single-ititle">Employment</h3>
+                        </div>
+
+                        <div class="single-timeline">
+                            <p class="years">2015-2016</p>
+                            <h4 class="timeline-title">UI/UX Designer</h4>
+                            <p class="timeline-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                        </div>
+                        <div class="single-timeline">
+                            <p class="years">2015-2016</p>
+                            <h4 class="timeline-title">Front-end developer</h4>
+                            <p class="timeline-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                        </div>
+                        <div class="single-timeline">
+                            <p class="years">2015-2016</p>
+                            <h4 class="timeline-title">WordPress Developer</h4>
+                            <p class="timeline-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!--  Resume Area Ended -->
 {!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
-{!! useDinamicStyle('containers') !!}
