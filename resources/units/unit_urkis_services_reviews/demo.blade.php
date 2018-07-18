19b3b895687b2e8f592e385f50ab698068048d96
sahakavatar <hakobyan.sahak88@gmail.com>
2018-07-18 11:20:45 +0400

    unit urkis

---
+++ resources/units/unit_urkis_services_reviews/demo.blade.php
@@ -0,0 +1,143 @@
+<!-- Service Area Starts -->
+<section class="service-area section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 service">
+                <header class="section-header text-center">
+                    <p class="slugan text-center">What can I do for you</p>
+                    <h2>Services</h2>
+                </header>
+
+                <div class="row services">
+                    <div class="col-sm-6">
+                        <div class="single-service">
+                            <div class="service-title d-flex justify-content-between align-items-center">
+                                <h3>UI/UX Design</h3>
+                                <span class="service-icon"><i class="far fa-lightbulb"></i></span>
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                                to make a type specimen book. </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="single-service">
+                            <div class="service-title d-flex justify-content-between align-items-center">
+                                <h3>Web development</h3>
+                                <span class="service-icon"><i class="fa fa-desktop"></i></span>
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                                to make a type specimen book. </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="single-service">
+                            <div class="service-title d-flex justify-content-between align-items-center">
+                                <h3>apps development</h3>
+                                <span class="service-icon"><i class="fa fa-mobile"></i></span>
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                                to make a type specimen book. </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="single-service">
+                            <div class="service-title d-flex justify-content-between align-items-center">
+                                <h3>graphic design</h3>
+                                <span class="service-icon"><i class="fa fa-cog"></i></span>
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                                to make a type specimen book. </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row service-counter">
+                    <div class="col-md-3 col-6 ">
+                        <div class="single-service-counter">
+                            <div class="scicon"><i class="far fa-gem"></i></div>
+                            <div class="ctext">
+                                <span class="counter-count">35</span>
+                                <p>project done</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-6 ">
+                        <div class="single-service-counter">
+                            <div class="scicon"><i class="fab fa-codepen"></i></div>
+                            <div class="ctext">
+                                <span class="counter-count">74</span>
+                                <p>happy clients</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-6 ">
+                        <div class="single-service-counter">
+                            <div class="scicon"><i class="fa fa-trophy"></i></div>
+                            <div class="ctext">
+                                <span class="counter-count">13</span>
+                                <p>awards won</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-6 ">
+                        <div class="single-service-counter">
+                            <div class="scicon"><i class="fa fa-coffee"></i></div>
+                            <div class="ctext">
+                                <span class="counter-count">45</span>
+                                <p>cups of coffee</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!--  Service Area Ended -->
+<!--  Reviews Area Starts -->
+<section class="reviews-area section-padding">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 col-md-10">
+                <header class="section-header text-center">
+                    <p class="slugan">What Others say about me</p>
+                    <h2>Reviews</h2>
+                </header>
+
+                <div id="MyReviewCorousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner reviews" role="listbox">
+                        <div class="carousel-item single-review active">
+                            <span class="review-icon"><i class="fa fa-quote-left"></i></span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci
+                                excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                            <span class="clients">Abdur Rahim, CEO of Theme-Valley</span>
+                        </div>
+
+                        <div class="carousel-item single-review">
+                            <span class="review-icon"><i class="fa fa-quote-left"></i></span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci
+                                excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                            <span class="clients">Abdur Rahim, CEO of Theme-Valley</span>
+                        </div>
+
+                        <div class="carousel-item single-review">
+                            <span class="review-icon"><i class="fa fa-quote-left"></i></span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci excepturi dolorum. Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore adipi sci
+                                excepturi dolorum. Lorem ipsum dolor sit amet, consectetur.</p>
+                            <span class="clients">Abdur Rahim, CEO of Theme-Valley</span>
+                        </div>
+                    </div>
+
+                    <ol class="carousel-indicators">
+                        <li data-target="#MyReviewCorousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#MyReviewCorousel" data-slide-to="1"></li>
+                        <li data-target="#MyReviewCorousel" data-slide-to="2"></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--  Reviews Area Ended -->
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}
