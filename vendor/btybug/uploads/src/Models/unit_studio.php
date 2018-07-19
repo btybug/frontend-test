d3d9de4387088b5e424ed84ec3e6e69b7617bdcb
Vardan <vardandanielyan@gmail.com>
2018-07-19 16:54:38 +0400

    New

---
+++ vendor/btybug/uploads/src/Models/unit_studio.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+namespace Btybug\Uploads\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class unit_studio extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'unit_studio';
+    /**
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+}
