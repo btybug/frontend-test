ac53fa61d1c6b663c45ca51e37c381466db422e8
terakyan <edo.terakyan@gmail.com>
2018-07-24 10:57:38 +0400

    layout

---
+++ vendor/btybug/mini/src/Resources/Views/layouts/front_pages.blade.php
@@ -0,0 +1,16 @@
+@extends("btybug::layouts.frontend")
+@section('contents')
+    @if($page->header)
+        @if($page->header_unit)
+            {!! render_mini_unit($page->header_unit,\Btybug\Mini\Model\MiniPainter::class) !!}
+        @else
+            {!! BBheader() !!}
+        @endif
+    @endif
+    
+    {!! BBRenderFrontLayout($page) !!}
+
+    @if($page->footer)
+        {!! BBfooter() !!}
+    @endif
+@stop
