da2d1e7b80403c2ca0825d93184192de10a1ae59
Vardan <vardandanielyan@gmail.com>
2018-08-03 10:45:57 +0400

    up

--- app/multisite/abo2/Resources/Views/mysite/settings.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('btybug::layouts.mTabs',['index'=>'mini_my_site_settings'])
-@section('tab')
-   <label>Select language</label>
-  <select>
-     <option>En</option>
-     <option>Ru</option>
-     <option>Am</option>
-  </select>
-@endsection
