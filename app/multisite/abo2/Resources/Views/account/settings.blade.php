9207fc096ece4835863f2a5d40658669f177ea1c
sahakavatar <hakobyan.sahak88@gmail.com>
2018-08-03 16:00:52 +0400

    up

--- app/multisite/abo2/Resources/Views/account/settings.blade.php
+++ app/multisite/abo2/Resources/Views/account/settings.blade.php
@@ -98,7 +98,7 @@
         <i class="fa fa-paint-brush bb-node-edit" bb-click="openStudioWindow"></i>
     </div>
 
-@section( 'CSS' )
+@section( 'css' )
     {!! HTML::style("public/css/formio/formio.full.min.css") !!}
     {!! HTML::style("public/css/formio/bootstrap.vertical-tabs.min.css") !!}
     {!! Html::style("public/css/form-builder/form-builder.css?m=m") !!}
@@ -145,7 +145,7 @@
 @stop
 
 
-@section( 'JS' )
+@section( 'js' )
     {!! HTML::script("public/js/formio/buttons.js") !!}
     {!! HTML::script("public/js/formio/formio.full.min.js") !!}
     {!! HTML::script("public/js/formio/config.js?v=".rand(999,9999)) !!}
