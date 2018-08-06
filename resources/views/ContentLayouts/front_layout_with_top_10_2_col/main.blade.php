abb4bad7c81b81757edb61b68d8a875af2bbdc2a
sahakavatar <hakobyan.sahak88@gmail.com>
2018-08-06 14:19:04 +0400

    layout

---
+++ resources/views/ContentLayouts/front_layout_with_top_10_2_col/main.blade.php
@@ -0,0 +1,15 @@
+    <header>
+
+    </header>
+    <div class="main">
+        <div class="row">
+            <div class="col-md-10 col-xs-12">
+
+            </div>
+            <div class="col-md-2 col-xs-12">
+
+            </div>
+        </div>
+    </div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
