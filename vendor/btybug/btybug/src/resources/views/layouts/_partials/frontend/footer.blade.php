73d8accfad62ea19955a1b9951bd6025f7c8bd03
sahakavatar <hakobyan.sahak88@gmail.com>
2018-08-10 15:13:39 +0400

    up

--- vendor/btybug/btybug/src/resources/views/layouts/_partials/frontend/footer.blade.php
+++ vendor/btybug/btybug/src/resources/views/layouts/_partials/frontend/footer.blade.php
@@ -1,12 +1,9 @@
 <!-- ================== BEGIN BASE FOOTER JS ================== -->
 @if($page->js_type == 'default')
-    {!! dd(BBgetProfileAssets(false,'js','footerJs')) !!}
     {!! BBgetProfileAssets(false,'js','footerJs') !!}
 @elseif($page->js_type == 'cms')
-    {!! dd(2) !!}
     {!! BBgetProfileAssets($page->js_cms,'js','footerJs') !!}
 @elseif($page->js_type == 'external')
-    {!! dd(3) !!}
     {!! BBlinkAssets($page->js) !!}
 @endif
 <!-- ================== END FOOTER JS ================== -->
