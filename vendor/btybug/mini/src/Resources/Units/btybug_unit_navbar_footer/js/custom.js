38f31bc0c5f053862619bcd4ae9b3a3758a30cf7
sahakavatar <hakobyan.sahak88@gmail.com>
2018-08-16 16:17:45 +0400

    closed fix

--- vendor/btybug/mini/src/Resources/Units/btybug_unit_navbar_footer/js/custom.js
+++ vendor/btybug/mini/src/Resources/Units/btybug_unit_navbar_footer/js/custom.js
@@ -3,10 +3,12 @@ $(document).ready(function () {
         $('.profiles-navbar .footerTabs').hide();
         $('.profile-footer .left .logo-name').removeClass('bg_dark');
     }
+
     function hidefavorite() {
         $('.profiles-navbar .profile-favorite').hide();
         $('.profile-footer .left .create .heart').removeClass('bg_dark');
     }
+
     $('body').on('click', '.profile-footer .right .icon', function () {
         if ($('.profile-footer .left').is(":hidden")) {
             $('.profile-footer .left').addClass('d-flex');
@@ -20,10 +22,15 @@ $(document).ready(function () {
         }
     });
 
-    $('body').on('click', '.profile-footer .left .logo-name', function () {
+    $('body').on('click', '.profile-footer .left .logo-name', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).toggleClass('active');
         if ($('.profiles-navbar .footerTabs').is(":hidden")) {
             $('.profiles-navbar .footerTabs').show();
             $(this).addClass('bg_dark');
+            $('.notification-panel').removeClass('active');
+            $('.profile-footer .left .icons a.message').removeClass('active');
             hidefavorite();
         } else {
             $('.profiles-navbar .footerTabs').hide();
@@ -44,17 +51,38 @@ $(document).ready(function () {
     });
 
 
-    $('.footerTabs .nav-item a').on('click',function () {
-        $('.footerTabs .nav-item a').each(function(i,elem) {
+    $('.footerTabs .nav-item a').on('click', function () {
+        $('.footerTabs .nav-item a').each(function (i, elem) {
             if ($(this).hasClass("active")) {
                 $(this).removeClass('active');
                 return false;
             }
         });
     });
+
+
     $('body').on('click', '.profile-footer .left .icons a.message', function (e) {
         e.preventDefault();
+        e.stopPropagation();
         $('.notification-panel').toggleClass('active');
+        $(this).toggleClass('active');
+        $('.profiles-navbar .footerTabs').hide();
+        $('.profile-footer .left .logo-name').removeClass('active bg_dark');
+    });
+
+    $(document).click(function (e) {
+        if ($('.profile-footer .left .icons a.message').hasClass('active') && e.target.closest('.notification-panel') != $('.notification-panel')[0]) {
+            $('.notification-panel').removeClass('active');
+            $('.profile-footer .left .icons a.message').removeClass('active');
+        }
+
+        if ($('.profile-footer .left .logo-name').hasClass('active') && e.target.closest('.profiles-navbar .footerTabs') != $('.profiles-navbar .footerTabs')[0]) {
+
+            $('.profiles-navbar .footerTabs').hide();
+            $('.profile-footer .left .logo-name').removeClass('active bg_dark');
+            console.log(55);
+        }
+
     });
 
 });
