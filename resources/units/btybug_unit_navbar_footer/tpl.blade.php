56f40d2e96fe74d53ad21a8463c8ee47cf4d9e88
terakyan <edo.terakyan@gmail.com>
2018-08-17 14:44:15 +0400

    up

--- resources/units/btybug_unit_navbar_footer/tpl.blade.php
+++ resources/units/btybug_unit_navbar_footer/tpl.blade.php
@@ -283,41 +283,41 @@
             <div class="login-reg-view">
                 <div class="images">
                     <img src="/public/images/dark-logo-bag.png" alt="img">
-                    <div class="row">
+                </div>
+                <div>
                     {!! Form::open(['url' => '/login']) !!}
-                        <fieldset>
-                            <h2>Please Login</h2>
-                            <hr class="colorgraph">
-                            <div class="form-group">
-                                <input type="text" name="usernameOremail" id="email" class="form-control input-lg"
-                                       placeholder="Username or Email">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" name="password" id="password" class="form-control input-lg"
-                                       placeholder="Password" value="">
-                            </div>
+                    <fieldset>
+                        <h2>Please Login</h2>
+                        <hr class="colorgraph">
+                        <div class="form-group">
+                            <input type="text" name="usernameOremail" id="email" class="form-control input-lg"
+                                   placeholder="Username or Email">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" id="password" class="form-control input-lg"
+                                   placeholder="Password" value="">
+                        </div>
 
-                            <div class="checkbox">
-                                <label>
-                                    <input name="remember" type="checkbox" value="Remember Me"> Remember Me
-                                </label>
-                                <a href="{!! url('forgot')!!}" class="btn btn-link pull-right">Forgot Password?</a>
-                            </div>
+                        <div class="checkbox">
+                            <label>
+                                <input name="remember" type="checkbox" value="Remember Me"> Remember Me
+                            </label>
+                            <a href="{!! url('forgot')!!}" class="btn btn-link pull-right">Forgot Password?</a>
+                        </div>
 
 
-                            <hr class="colorgraph">
-                            <div class="row">
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    <input type="submit" class="btn btn-lg btn-success btn-block" value="Login">
-                                </div>
+                        <hr class="colorgraph">
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <input type="submit" class="btn btn-lg btn-success btn-block" value="Login">
                             </div>
-                        </fieldset>
+                        </div>
+                    </fieldset>
                     {!! Form::close() !!}
-                    </div>
+                </div>
 
-                    <div class="row">
-                        <a href="{!! url('register')!!}" class="btn btn-lg btn-primary btn-block">Not member ...Register</a>
-                    </div>
+                <div>
+                    <a href="{!! url('register')!!}" class="btn btn-lg btn-primary btn-block">Not member ...Register</a>
                 </div>
             </div>
         </div>
