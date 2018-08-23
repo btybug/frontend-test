500e2abf6e85bb67912f12aa6025b1753f00c81c
Sahak <hakobyan.sahak88@gmail.com>
2018-08-23 11:42:17 +0400

    up

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 */
 $users=\Btybug\User\User::all()->pluck('id')->toArray();
 
-dd($users,\Btybug\FrontSite\Models\FrontendPage::wherNotIn('user_id',$users)->delete());
+dd($users,\Btybug\FrontSite\Models\FrontendPage::whereNotIn('user_id',$users)->delete());
 Route::get('/', function () {
     return view('welcome');
 });
