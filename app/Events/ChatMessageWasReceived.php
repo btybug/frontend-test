8ee0149aff6a899e6f827905f9c2733d36b4a7c0
Sahak <hakobyan.sahak@gmail.com>
2018-08-28 23:22:22 +0400

    up

--- app/Events/ChatMessageWasReceived.php
+++ app/Events/ChatMessageWasReceived.php
@@ -2,7 +2,6 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -22,14 +21,14 @@ class ChatMessageWasReceived  implements ShouldBroadcastNow
         $this->chatMessage = $chatMessage;
         $this->user = $user;
 
-        event(new MessagePushed('ewqererqwe'));
+
 
     }
 
     public function broadcastOn()
     {
 
-       // return new PresenceChannel('socket.io' . $this->survey->id);
+//        return new PresenceChannel('socket.io' . $this->survey->id);
 //        return dd(new PresenceChannel('socket.io'));
 
         echo 789;
