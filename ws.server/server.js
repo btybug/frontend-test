48bb2582405d63fcf396641676f58e8b93328b27
Sahak <hakobyan.sahak@gmail.com>
2018-08-29 16:04:08 +0400

    update

--- ws.server/server.js
+++ ws.server/server.js
@@ -5,7 +5,6 @@ var io=require('socket.io')(6001),
     Redis=require('ioredis'),
     redis=new Redis();
 redis.psubscribe('*',function (error,count) {
-    
 });
 redis.on('pmessage',function (pattern,channel,message) {
     message=JSON.parse(message);
