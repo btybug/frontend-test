e05136cbadab7765cef16b1414b0ff372599f489
Sahak <hakobyan.sahak@gmail.com>
2018-08-31 11:25:28 +0400

    up

--- vendor/btybug/frontsite/src/Resources/Views/frontend/pages/_partials/bug_render.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/frontend/pages/_partials/bug_render.blade.php
@@ -211,7 +211,7 @@
                                                     <div class="container-fluid">
                                                         <div class="row">
                                                             <div class="col-md-2">
-                                                                <img src="{!! $comment->author->avatar !!}" alt="">
+                                                                <img src="{!! $comment->author->socialProfile->site_image !!}" alt="">
                                                             </div>
                                                             <div class="col-md-10">
                                                                 <div class="name">
